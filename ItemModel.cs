2d1e10f88c5982d4d1fa07dee4c022fefb38a78a
katherineESL <katherine.hanson@electricsystemsltd.com>
2022-10-19 21:07:35 +0100

    both offer price and offer condition nullable as can be blank

--- ItemModel.cs
+++ ItemModel.cs
@@ -5,7 +5,8 @@ public class ItemModel
 		public string Sku {get;set;}
         public decimal Price{get; set;}
         public int Qty { get; set; }
-        public decimal OfferPrice { get; set; }
+        public decimal? OfferPrice { get; set; }
+        public decimal? OfferCondition { get; set; }
         
 }
 //model for Items scanned
