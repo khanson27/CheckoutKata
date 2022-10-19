e064335760ee93c9540ad6916caaa8102dba9600
katherineESL <katherine.hanson@electricsystemsltd.com>
2022-10-19 21:09:16 +0100

    implemented scan functionality, added new item as new instance due

--- Checkout.cs
+++ Checkout.cs
@@ -12,9 +12,32 @@ public class Checkout
         //if offer price is not null, add all prices and offer prices
         return 0m;
     }
+    
+        public void Scan(ItemModel item)
+        {
+            if (ItemList?.Count > 0 && ItemList.Any(i => i.Sku == item.Sku))
+            {
+                int existingItemIndex = ItemList.FindIndex(i => i.Sku == item.Sku);
+                 ItemList[existingItemIndex].Qty += item.Qty;
+            }
+            else
+            {
+                ItemModel newItem = new ItemModel
+                {
+                    Sku = item.Sku, Price = item.Price, Qty = item.Qty, OfferCondition = item.OfferCondition,
+                    OfferPrice = item.OfferPrice
+                };
+                ItemList.Add(newItem);
+            }
+            //1st item add to list and +1 to qty
+            //if item is already in list, +1 to qty
+           
+        
+        }
+    
  
-    public void Scan(ItemModel items)
-    {
+
+//if qty is is divisible by offer price and has matching SKU use offer price * remainder unless 0 
         //1st item add to list and +1 to qty
         //if item is already in list, +1 to qty
         //if qty reaches offer level, add offer price, set price to null/0
