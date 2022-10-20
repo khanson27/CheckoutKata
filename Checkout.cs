cdea5e4b266ca21c609bb13842fbdcd608485526
katherineESL <katherine.hanson@electricsystemsltd.com>
2022-10-20 18:44:04 +0100

    removed comments

--- Checkout.cs
+++ Checkout.cs
@@ -60,22 +60,9 @@ public class Checkout
         
         }
     
- 
-
-//if qty is is divisible by offer price and has matching SKU use offer price * remainder unless 0 
-        //1st item add to list and +1 to qty
-        //if item is already in list, +1 to qty
-        //if qty reaches offer level, add offer price, set price to null/0
+    
         
     }
 
-
-
-//if qty is is divisible by offer price and has matching SKU use offer price * remainder unless 0 
-/*public class SpecialOffer
-{
-    public int Qty { get; set; }
-    public decimal OfferPrice { get; set; }
-}*/
 
 
