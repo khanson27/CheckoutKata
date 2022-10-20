0417cb0497d98f9b3757daca52cbd702d570fc01
katherineESL <katherine.hanson@electricsystemsltd.com>
2022-10-20 18:43:27 +0100

    set up testing framework

---
+++ Tests/CheckoutKataTests.cs
@@ -0,0 +1,20 @@
+
+using CheckoutKata;
+
+namespace Tests;
+
+public class CheckoutKataTests
+{
+    //set up
+    public Checkout checkout = new Checkout();
+
+   
+    
+    
+    [Theory]
+    [InlineData()
+    public void Scan_WithItemNotInList_AddsItemAndQtyToList (ItemModel item)
+    {
+      
+    }
+}
